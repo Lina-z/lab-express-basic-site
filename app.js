abf668720328bf31ee61cfe811db34c3eb5bea52
Lina Zerkly <zerkly.lina@gmail.com>
2023-05-30 17:05:12 +0200

    structure + img

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+
+app.get("/home", (req, res, next) => {
+	res.sendFile(__dirname + "/views/home-page.html");
+});
+
+app.get("/about", (req, res, next) => {
+	res.sendFile(__dirname + "/views/about-page.html");
+});
+
+app.get("/gallery", (req, res, next) => {
+	res.sendFile(__dirname + "/views/gallery-page.html");
+});
+
+app.get("/works", (req, res, next) => {
+	res.sendFile(__dirname + "/views/works-page.html");
+});
+
+app.listen(3000, () => { console.log("server listening on port 3000...")});
